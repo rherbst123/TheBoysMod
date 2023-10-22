0707c60249b059f837801934eff0d64fd33e5b61
rherbst123 <57201041+rherbst123@users.noreply.github.com>
2023-10-22 12:54:43 -0500

    okay

---
+++ forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/theboysmod/Init/moditems/thepenItem.java
@@ -0,0 +1,11 @@
+package dev.riley.theboysmod.Init.moditems;
+
+import net.minecraft.world.item.BowItem;
+
+public class thepenItem extends BowItem {
+    public thepenItem(Properties properties){
+        super(properties);
+    }
+
+
+}
