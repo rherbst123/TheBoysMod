303aabdb24607df437b4d12845f4b57c296fb431
rileyherbst <rileyherbst@yahoo.com>
2023-11-01 12:11:32 -0500

    idk

---
+++ forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/theboysmod/Init/SoundInit.java
@@ -0,0 +1,4 @@
+package dev.riley.theboysmod.Init;
+
+public class SoundInit {
+}
--- forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/theboysmod/Init/moditems/thepenItem.java
+++ forge-1.20.1-47.2.0-mdk/src/main/java/dev/riley/theboysmod/Init/moditems/thepenItem.java
@@ -1,11 +1,23 @@
 package dev.riley.theboysmod.Init.moditems;
 
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class thepenItem extends BowItem {
     public thepenItem(Properties properties){
+
         super(properties);
     }
 
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
 
+
+
+        return super.use(level,player,hand);
+    }
 }
